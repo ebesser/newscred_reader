dc2e28bfcf808ac8a02769508fa3b5ea315f9339
Arti Villa <artivillachandok@gmail.com>
2015-10-01 17:09:05 -0400

    add ibm logo and some view stuff

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,10 @@
+$(window).load(function() {      //Do the code in the {}s when the window has loaded
+
+	var t = 2;
+	setTimeout(function(){
+		// $("#loader").fadeOut("fast");  //Show NewsCred screen for a t seconds
+		// $("#lists").fadeIn("fast");
+	}, t * 1000);
+
+	console.log('pandas');
+});
